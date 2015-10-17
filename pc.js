a8f25e4cc4b06066829e86937a2b9f4ae128d0fd
CS Tan <ctan@anzgcis.com>
2015-10-17 14:27:49 +1100

    hello word

--- pc.js
+++ pc.js
@@ -1,41 +1,45 @@
 var attributes = [
 	// Environ
-	"desert", "tundra", "mountain", "space", "field", "urban",
+	//"desert", "tundra", "mountain", "space", "field", "urban",
 	// Stealth and cunning
-	"hidden", "covert", "uncanny", "scheming", "decisive",
+	//"hidden", "covert", "uncanny", "scheming", "decisive",
 	// Volitility
-	"rowdy", "dangerous", "explosive", "threatening", "warring",
+	//"rowdy", "dangerous", "explosive", "threatening", "warring",
 	// Needs correction
-	"bad", "unnecessary", "unknown", "unexpected", "waning", 
+	//"bad", "unnecessary", "unknown", "unexpected", "waning",
 	// Organic Gems and materials
-	"amber", "bone", "coral", "ivory", "jet", "nacre", "pearl", "obsidian", "glass",
+	//"amber", "bone", "coral", "ivory", "jet", "nacre", "pearl", "obsidian", "glass",
 	// Regular Gems
-	"agate", "beryl", "diamond", "opal", "ruby", "onyx", "sapphire", "emerald", "jade",
+	//"agate", "beryl", "diamond", "opal", "ruby", "onyx", "sapphire", "emerald", "jade",
 	// Colors
-	"red", "orange", "yellow", "green", "blue", "violet",
+	//"red", "orange", "yellow", "green", "blue", "violet",
+	// etc
+	"Explosive", "Smashing", "Crouching", "Crazy", "Boiling", "Burning", "Rolling", "Sexy", "Shaking", "Red"
 ]
 
 var objects = [
 	// Large cats
-	"panther", "wildcat", "tiger", "lion", "cheetah", "cougar", "leopard",
+	//"panther", "wildcat", "tiger", "lion", "cheetah", "cougar", "leopard",
 	// Snakes
-	"viper", "cottonmouth", "python", "boa", "sidewinder", "cobra",
+	//"viper", "cottonmouth", "python", "boa", "sidewinder", "cobra",
 	// Other predators
-	"grizzly", "jackal", "falcon",
+	//"grizzly", "jackal", "falcon",
 	// Prey
-	"wildabeast", "gazelle", "zebra", "elk", "moose", "deer", "stag", "pony",
+	//"wildabeast", "gazelle", "zebra", "elk", "moose", "deer", "stag", "pony",
 	// HORSES!
-	"horse", "stallion", "foal", "colt", "mare", "yearling", "filly", "gelding",
+	//"horse", "stallion", "foal", "colt", "mare", "yearling", "filly", "gelding",
 	// Occupations
-	"nomad", "wizard", "cleric", "pilot",
+	//"nomad", "wizard", "cleric", "pilot",
 	// Technology
-	"mainframe", "device", "motherboard", "network", "transistor", "packet", "robot", "android", "cyborg",
+	//"mainframe", "device", "motherboard", "network", "transistor", "packet", "robot", "android", "cyborg",
 	// Sea life
-	"octopus", "lobster", "crab", "barnacle", "hammerhead", "orca", "piranha",
+	//"octopus", "lobster", "crab", "barnacle", "hammerhead", "orca", "piranha",
 	// Weather
-	"storm", "thunder", "lightning", "rain", "hail", "sun", "drought", "snow",
+	//"storm", "thunder", "lightning", "rain", "hail", "sun", "drought", "snow",
 	// Other
-	"warning", "presence", "weapon"
+  //	"warning", "presence", "weapon"
+  // etc
+	"Thunder", "Cougar", "Lobster", "Pumpkins", "Mama", "Tiger", "Wildcat", "Pirahrna", "Cobra"
 ]
 
 var type = "PROJECT";
@@ -77,6 +81,14 @@ function updateTwitterButton()
 	{
 		twttr.widgets.load();
 	}
+}
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
 }
 
 createCodename();
